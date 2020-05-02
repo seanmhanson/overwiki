b8fc20f5d4b13e3fc80512bb89f866fc27afcf76
Sean Hanson <smh@seanmhanson.com>
2020-05-02 18:14:42 -0400

    test commit

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const apiPort = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
