ca5b238783b7535def77c8eeb18d77bd08ec40a9
Sean Hanson <smh@seanmhanson.com>
2020-05-03 20:50:29 -0400

    Add clientside using create-react-app and pare down

---
+++ client/src/test/App.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../app/App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const testElement = getByText('Hello World!');
+  expect(testElement).toBeInTheDocument();
+});
